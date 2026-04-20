c2737d89a8674bbb7326333e700992298e489196
vladimircvetov99-web <vladimir.cvetov.99@gmail.com>
2026-04-20 16:26:42 +0600

    Make all website pages adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -29,7 +29,52 @@ $(function () {
       slidesToShow: 10,
       slidesToScroll: 10,
       dots: true,
-      arrows: false
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 1700,
+          settings: {
+             slidesToShow: 8,
+             slidesToScroll: 8,
+          }
+        },
+        {
+          breakpoint: 1511,
+          settings: {
+             slidesToShow: 6,
+             slidesToScroll: 6,
+          }
+        },
+        {
+          breakpoint: 1201,
+          settings: {
+            slidesToShow: 4,
+             slidesToScroll: 4,
+          }
+        },
+         {
+          breakpoint: 841,
+          settings: {
+            slidesToShow: 3,
+             slidesToScroll: 3,
+          }
+        },
+          {
+          breakpoint: 551,
+          settings: {
+            slidesToShow: 2,
+             slidesToScroll: 2,
+          }
+        },
+          {
+          breakpoint: 376,
+          settings: {
+            slidesToShow: 1,
+             slidesToScroll:1,
+          }
+        },
+        
+      ]
     });
 
    $('.article-slider__box').slick({
